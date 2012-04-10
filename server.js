ddaa93d420d3e6307d0e0109fe48dc4d99158634
Hugh Kennedy <hughskennedy@gmail.com>
2012-04-10 14:52:23 +1000

    works with connect req.body, version bump 0.0.1 -> 0.0.2

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var http = require('http'),
 	connect = require('connect');
 
 // Vanilla Server
-// http.createServer(function(req, res) {
-// 	gate(req, function(err, fields) {
-// 		res.end(JSON.stringify(fields));
-// 	});
-// }).listen(8212);
+http.createServer(function(req, res) {
+	gate(req, function(err, fields) {
+		res.end(JSON.stringify(fields));
+	});
+}).listen(8212);
 
 // Connect Server
+/*
+
 connect.createServer(function(req, res) {
 	gate(req, function(err, fields) {
 		res.end(JSON.stringify(fields));
 	});
 }).listen(8212);
+
+*/
